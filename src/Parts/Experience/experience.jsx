00a9ba36ecbe3e82df70ef2ae4cb773bf89dfdf7
ShayyanAsim <shayyan.asim@ucalgary.ca>
2023-11-14 01:22:24 -0700

    THIS CSS TOOK A WHILE

--- src/Parts/Experience/experience.jsx
+++ src/Parts/Experience/experience.jsx
@@ -1,8 +1,103 @@
 import React from 'react'
+import './experience.css'
+import {IoMdCheckmarkCircle} from 'react-icons/io'
 
 const experience = () => {
   return (
-    <section id='experience'>experience</section>
+    <section id='experience'>
+      <h2>My Skills</h2>
+
+      <div className='container experience__container'>
+        <div className='BIGBOX'>
+          <div className='BOXCONTENT'>
+            <article className='BOXDETAILS'>
+              <IoMdCheckmarkCircle/>
+              <h4>HTML</h4>
+              </article>
+
+
+          
+              <article className='BOXDETAILS'>
+              <IoMdCheckmarkCircle/>
+              <h4>CSS</h4>
+              </article>
+
+
+
+              <article className='BOXDETAILS'>
+              <IoMdCheckmarkCircle/>
+              <h4>React</h4>
+              </article>
+
+
+
+              <article className='BOXDETAILS'>
+              <IoMdCheckmarkCircle/>
+              <h4>AWS</h4>
+              </article>
+
+
+
+              <article className='BOXDETAILS'>
+              <IoMdCheckmarkCircle/>
+              <h4>C++</h4>
+              </article>
+
+
+              <article className='BOXDETAILS'>
+              <IoMdCheckmarkCircle/>
+              <h4>C</h4>
+              </article>
+
+
+              <article className='BOXDETAILS'>
+              <IoMdCheckmarkCircle/>
+              <h4>Python</h4>
+              </article>
+
+
+              <article className='BOXDETAILS'>
+              <IoMdCheckmarkCircle/>
+              <h4>Java</h4>
+              </article>
+
+
+              <article className='BOXDETAILS'>
+              <IoMdCheckmarkCircle/>
+              <h4>Object Oriented Programming</h4>
+              </article>
+
+
+              <article className='BOXDETAILS'>
+              <IoMdCheckmarkCircle/>
+              <h4>Terraform</h4>
+              </article>
+
+              <article className='BOXDETAILS'>
+              <IoMdCheckmarkCircle/>
+              <h4>Regression/Sanity Testing</h4>
+              </article>
+
+              <article className='BOXDETAILS'>
+              <IoMdCheckmarkCircle/>
+              <h4>API Integration</h4>
+              </article>
+
+
+
+
+          </div>
+        </div>
+
+      </div>
+    
+    
+    
+    
+    
+    
+    </section>
+   
   )
 }
 
